b9c05401bbeb58b981e81ae00b19b6887fc4f1cb
briannakeune <briannakeune@gmail.com>
2019-08-14 16:56:32 -0500

    got things appering in browser

--- src/components/ApodCard.js
+++ src/components/ApodCard.js
@@ -3,7 +3,14 @@ import React from 'react';
 const ApodCard = props => {
     return (
         <div className="apod">
-            <p>Hello I am APOD Card.</p>
+            <h1>Astronomy Picture of the Day (APOD)</h1>
+            <img className="apodImg" alt={props.title} src={props.imgUrl} />
+            <div>
+                <h2 className="picTitle">{props.title}</h2>
+                {/* <h3 className="imgCred"></h3> */}
+                <p className="explanation">{props.expl}</p>
+                {/* <p className="tomorrowPod"></p> */}
+            </div>
         </div>
     );
 };
--- src/components/ApodContent.js
+++ src/components/ApodContent.js
@@ -7,16 +7,27 @@ export default function ApodContent() {
 
     useEffect(() => {
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-    })
-    .then(resp => {
-        const pod = resp;
-        console.log(`Data needed`, pod);
-    })
-
+        .then(resp => {
+            //pod = picture of the day
+            const pod = resp.data;
+            console.log(`Data needed`, pod);
+            setContent(pod);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    
+    }, []);
     return(
-        <div className="content">
-            <p>poop.</p>
-
+        <div className="apodContent">
+            <ApodCard 
+            key={content.url}
+            id={content.url}
+            title={content.title}
+            imgUrl={content.url}
+            expl={content.explanation}
+            date={content.date}
+            />
         </div>
     );
 };
