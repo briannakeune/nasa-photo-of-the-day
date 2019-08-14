c39cea84781ad4f1427eb8b237d1f56274ec2611
briannakeune <briannakeune@gmail.com>
2019-08-14 18:55:20 -0500

    have components set up the way I think I want I may need to change layout in app js also tried adding less files to project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import ApodContent from  './components/ApodContent';
 import Header from './components/header/Header'
+import FooterNav from './components/footer/FooterNav'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ApodContent />
+      <FooterNav />
     </div>
   );
 }
---
+++ src/components/footer/FooterNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function FooterNav() {
+    return(
+        <div className="footerNav">
+            <p className="author">Author &amp; Editors: <a href={"https://www.mtu.edu/physics/department/faculty/nemiroff/"}>Robert J. Nemiroff</a>(<a href={"https://www.mtu.edu/physics/"}>MTU</a>) &amp; <a href={"https://apod.nasa.gov/htmltest/jbonnell/www/bonnell.html"}>Jerry Bonnell</a> (<a href={"http://www.astro.umd.edu/"}>UMCP</a>)</p>
+            <p className="official">NASA Official: <a href={"https://apod.nasa.gov/apod/lib/about_apod.html#srapply"} className="privacyPolicy">Specifics rights apply.</a></p>
+            <p className="serviceOf">A service of: <a href={"https://science.gsfc.nasa.gov/astrophysics/"}>ASD</a> at  <a href={"https://www.nasa.gov/"}>NASA</a>/<a href={"https://www.nasa.gov/goddard"}>GSFC</a> &amp; <a href={"https://www.mtu.edu/"}>Michigan Tech. U.</a></p>
+            <a href={"https://www.nasa.gov/about/highlights/HP_Privacy.html"} className="privacyPolicy">NASA WEB Privacy Policy</a>
+        </div>
+    )
+}
