011c665f488065274e414aee78d36895a6c72900
briannakeune <briannakeune@gmail.com>
2019-08-15 16:51:55 -0500

    used semantic ui for image and card also used style component for logo

--- src/components/ApodCard.js
+++ src/components/ApodCard.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import {Card, Image} from "semantic-ui-react";
+
 
 const ApodCard = props => {
     return (
-        <div className="apod" key={props.id}>
-            <h1>Astronomy Picture of the Day (APOD)</h1>
-            <img className="apodImg" alt={props.title} src={props.imgUrl} />
-            <div className="imgText">
-                <h2 className="picTitle">{props.title}</h2>
-                {/* <h3 className="imgCred"></h3> */}
-                <p className="explanation">{props.expl}</p>
-                {/* <p className="tomorrowPod"></p> */}
-            </div>
+        <div>
+          <h2>Astronomy Picture of the Day (APOD)</h2>
+            <Image src={props.imgUrl} centered />
+        <Card>
+          {/* <Image src="{props.imgUrl}" alt="{props.title}" wrapped ui={false} /> */}
+          <Card.Content>
+            <Card.Header>{props.title}</Card.Header>
+            <Card.Meta>
+              <span className='date'>{props.date}</span>
+            </Card.Meta>
+            <Card.Description>
+                {props.expl}
+            </Card.Description>
+          </Card.Content>
+        </Card>
         </div>
+
     );
 };
 
